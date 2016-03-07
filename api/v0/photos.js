8074ba98b52637941dc31519f21b4dd4c0ac132b
otnt <jiangpufan@gmail.com>
2016-03-06 18:12:29 -0800

    photo upload base64 data

--- api/v0/photos.js
+++ api/v0/photos.js
@@ -4,6 +4,9 @@ var express = require("express");
 var router = express.Router();
 var models = require("../../database/v0/models.js");
 var bodyParser = require('body-parser');
+var AWS = require('aws-sdk');
+AWS.config.loadFromPath('./crenditial.json');
+var s3 = new AWS.S3();
 
 var helper = require("./helper.js");
 var errHandle = helper.errHandle;
@@ -18,11 +21,13 @@ router.route('/:id')
   })
 });
 
+router.use(bodyParser.json({limit: '50mb'}));
 router.route('/')
-.post(bodyParser.json(),
-function(req, res, next) {
-  var fakeUserId;
+.post(function(req, res, next) {
+  var buf = req.body.data;
+  if(!buf) return errHandle.badRequest(res, "Need image data");
 
+  var fakeUserId;
   var getFakeUserId = function() {
     models.User.findOne(function(err, user) {
       if(err) return errHandle.unknown(res, err);
@@ -33,20 +38,46 @@ function(req, res, next) {
 
   var postPhoto = function() {
     var data = req.body;
-    var imageUrl = data.imageUrl;
-    
-    var referenceId = data.referenceId;
-    //var ownerId = fakeUserId;
     data.ownerId = fakeUserId;
-    var tagList = data.tagList;
-
-    if(!imageUrl) return errHandle.badRequest(res, "Need imageUrl for photo");
 
     models.Photo.create(data, function(err, photo) {
       if(err) return errHandle.unknown(res, err);
-      res.statusCode = 201;
-      res.send(photo);
+      uploadImage(''+photo._id, updatePhoto);
     });
+  }
+
+  var uploadImage = function(key, callback) {
+    //get base64 data
+    buf = new Buffer(buf, 'base64');
+
+    var params = {
+      Bucket: 'copycatimage',
+      Key: key,
+      ACL: 'public-read',
+      Body: buf,
+      ContentEncoding: 'base64',
+      ContentLength: buf.length,
+      ContentType: 'image/jpeg'
+    };
+    s3.upload(params)
+    .send(function(err, data) {
+      if (err) return errHandle.unknown(res, err);
+      callback(data.key, data.Location);
+    });
+  }
+
+  var updatePhoto = function(id, url) {
+    models.Photo.findByIdAndUpdate(
+      id, 
+      { $set: { imageUrl : url}},
+      complete
+    );
+  }
+
+  var complete = function(err, photo) {
+    if(err) return errHandle.unknown(res, err);
+    res.statusCode = 201;
+    res.send(photo);
   }
 
   getFakeUserId();
--- database/v0/photo.js
+++ database/v0/photo.js
@@ -6,10 +6,12 @@ var ObjectId = mongoose.Schema.ObjectId;
 
 module.exports = mongoose.model('Photo', 
   mongoose.Schema({
-      imageUrl: {type: String, required: true},
+      imageUrl: {type: String},
       referenceId: {type: ObjectId, ref: 'Photo'},
       ownerId: {type: ObjectId, ref: 'User', required: true},
       tagList: [String],
-      time : { type : Date, default: Date.now, required: true}
+      time : { type : Date, default: Date.now, required: true},
+      length:{type:Number},
+      width:{type:Number}
   })
 );
