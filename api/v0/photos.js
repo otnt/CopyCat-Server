7aeb1141d7ce5cf3e7e9c41c2a1aafafa1e3bcc9
otnt <jiangpufan@gmail.com>
2016-03-24 22:28:01 -0400

    vagrant

--- api/v0/photos.js
+++ api/v0/photos.js
@@ -1,14 +1,18 @@
-const express = require("express");
-const router = express.Router();
-const models = require("../../database/v0/models.js");
-const bodyParser = require('body-parser');
-const AWS = require('aws-sdk');
+'use strict';
+
+var express = require("express");
+var router = express.Router();
+var models = require("../../database/v0/models.js");
+var bodyParser = require('body-parser');
+var AWS = require('aws-sdk');
 AWS.config.loadFromPath('./crenditial.json');
-const s3 = new AWS.S3();
+var s3 = new AWS.S3();
+
+var base64 = require('base64-stream');
+var zlib = require('zlib');
 
 var helper = require("./helper.js");
 var errHandle = helper.errHandle;
-var assertHeader = helper.assertHeader;
 
 var fs = require('fs')
   , gm = require('gm').subClass({imageMagick: true});
@@ -21,53 +25,43 @@ router.route('/:id')
     if(!photo) return errHandle.notFound(res, err);
     res.send(photo);
   })
-})
-.put(bodyParser.json({limit: '5mb'}), 
-function(req, res, next) {
-  if(!assertHeader(req.get('content-type'), 'application/json', 'content-type', res)) return;
+});
 
+router.route('/:id/stream')
+.put(function(req, res, next) {
   var id = req.params.id;
-  var dataBase64 = req.body.dataBase64;
-  if(!dataBase64) return errHandle.badRequest(res, "Need image data");
-  var buf = new Buffer(dataBase64, 'base64');
-
-  var compress = function() {
-    gm(buf, 'img')
-    .resize(800, 600)
-    .quality(80)
-    .toBuffer('JPG',function (err, buffer) {
-      if (err) return errHandle.unknown(res, err);
-      uploadImage(buffer);
-    });
+  var fakeUserId;
+  var getFakeUserId = function() {
+    models.User.findOne(function(err, user) {
+      if(err) return errHandle.unknown(res, err);
+      fakeUserId = user._id;
+      uploadImage(id, updatePhoto);
+    })
   }
 
-  var uploadImage = function(buffer) {
+  var uploadImage = function(id, callback) {
     var params = {
-      Bucket: 'copycattest',
+      Bucket: 'copycatimage',
       Key: id,
       ACL: 'public-read',
-      Body: buffer,
+      Body: req.pipe(base64.decode()).pipe(zlib.createGzip()),
+      ContentEncoding: 'gzip',
       ContentType: 'image/jpeg'
     };
     s3.upload(params)
     .send(function(err, data) {
       if (err) return errHandle.unknown(res, err);
-      updatePhoto(data.Location);
+      callback(id, data.Location);
     });
   }
 
-  //get image size info and update photo
-  var updatePhoto = function(url) {
-    gm(buf, 'img')
-    .size(function(err, size){
-      if(err) return errHandle.unknown(res, err);
-      models.Photo.findByIdAndUpdate(
-        id, 
-        { $set: { imageUrl : url, width : size.width, height : size.height}},
-        {new : true},//set true to return modified data
-        complete
-      );
-    })
+  var updatePhoto = function(id, url) {
+    models.Photo.findByIdAndUpdate(
+      id, 
+      { $set: { imageUrl : url}},
+      {new : true},//set true to return modified data
+      complete
+    );
   }
 
   var complete = function(err, photo) {
@@ -76,13 +70,15 @@ function(req, res, next) {
     res.send(photo);
   }
 
-  //compress -> upload image to s3 -> update photo in database -> complete
-  compress();
+  getFakeUserId();
 });
 
+router.use(bodyParser.json({limit: '50mb'}));
 router.route('/')
-.post(bodyParser.json(),
-function(req, res, next) {
+.post(function(req, res, next) {
+  var buf = req.body.data;
+  //if(!buf) return errHandle.badRequest(res, "Need image data");
+
   var fakeUserId;
   var getFakeUserId = function() {
     models.User.findOne(function(err, user) {
@@ -92,18 +88,65 @@ function(req, res, next) {
     })
   }
 
+  var key;
   var postPhoto = function() {
     var data = req.body;
     data.ownerId = fakeUserId;
 
     models.Photo.create(data, function(err, photo) {
       if(err) return errHandle.unknown(res, err);
-      res.statusCode = 201;
-      res.send(photo);
+      //put image info and data at same time
+      if(buf) {
+        key = '' + photo._id;
+        compress();
+      }
+      //first post image, then put data
+      else {
+        complete(null, photo);
+      }
     });
   }
 
-  //get user id -> post photo without image data
+  var compress = function() {
+    //get base64 data
+    buf = new Buffer(buf, 'base64').toString('byte');
+
+    gm(buf, 'buffer')
+    .thumb(800, 600, './' + key, 80, uploadImage);
+  }
+
+  var uploadImage = function(err, stdout, stderr, command) {
+    var params = {
+      Bucket: 'copycatimage',
+      Key: fs.createReadStream(),
+      ACL: 'public-read',
+      Body: buf,
+      ContentEncoding: 'base64',
+      ContentLength: buf.length,
+      ContentType: 'image/jpeg'
+    };
+    s3.upload(params)
+    .send(function(err, data) {
+      if (err) return errHandle.unknown(res, err);
+      uploadPhoto(data.key, data.Location);
+    });
+  }
+
+  var updatePhoto = function(id, url) {
+    models.Photo.findByIdAndUpdate(
+      id, 
+      { $set: { imageUrl : url}},
+      {new : true},//set true to return modified data
+      complete
+    );
+  }
+
+  var complete = function(err, photo) {
+    if(err) return errHandle.unknown(res, err);
+    res.statusCode = 201;
+    res.send(photo);
+  }
+
   getFakeUserId();
 });
 module.exports = router;
