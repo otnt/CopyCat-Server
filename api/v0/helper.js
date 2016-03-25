a697b4c8cd8fdb820c8eff4abf420041c2edac7f
otnt <jiangpufan@gmail.com>
2016-03-25 16:34:00 -0400

    1.better logger, 2.promise made more readable

--- api/v0/helper.js
+++ api/v0/helper.js
@@ -1,5 +1,9 @@
 'use strict';
 
+/**
+ * Populate photo id list to album with restriction.
+ */
+
 var prefetchPhotoNumber = 10;
 module.exports.photoIdListPopulate = {
   path: 'photoIdList',
@@ -7,18 +11,28 @@ module.exports.photoIdListPopulate = {
   options: {limit : prefetchPhotoNumber}
 }
 
-module.exports.assertHeader = function(req, res, name, wanted) {
+/**
+ * Assert http request header given name and wanted field value
+ */
+
+module.exports.assertHeader = function(req, res, log, name, wanted) {
   var actual = req.get(name);
   if((wanted !== '*') && (wanted.split("|").indexOf(actual) === -1)) {
     res.status(400).send({'errCode':400, 'errMsg':"Bad request: " + 
           name + " should be '" + wanted + "' but got " + "'" + actual + "'"
     });
+    log.info("Assert header %s, wanted: %s, actual: %s, failed", name, wanted, actual);
     return false;
   }
+  log.info("Assert header %s, wanted: %s, actual: %s, passed", name, wanted, actual);
   return true;
 }
 
-//err handling
+/**
+ * Error handling.
+ * Given error type, respond object.
+ */
+
 var errHandle = function() {}
 errHandle.notFound = function(res, err) {
   res.status(404).send({'errCode':404, 'errMsg' : "Not found: " + err});
@@ -30,3 +44,110 @@ errHandle.badRequest = function(res, err) {
   res.status(400).send({'errCode':400, 'errMsg': "Bad request: " + err});
 }
 module.exports.errHandle = errHandle;
+
+
+/**
+ * Logger system.
+ */
+
+var bunyan = require('bunyan');
+var uuid = require('node-uuid');
+
+function photoSerializer(photo) {
+  return {
+    id: photo._id,
+    imageUrl: photo.imageUrl,
+    referenceId: photo.referenceId,
+  };
+}
+function editorSerializer(editor) {
+  return {
+    id: editor._id,
+    name: editor.name,
+  }
+}
+function albumSerializer(album) {
+  return {
+    id: album._id,
+    name: album.name,
+    imageUrl: album.imageUrl,
+  }
+}
+function userSerializer(user) {
+  return {
+    id: user._id,
+    name: user.name,
+  }
+}
+function reqSerializer(req) {
+  return {
+    method: req.method,
+    url: req.originalUrl,
+  }
+}
+function resSerializer(res) {
+  return {
+    statusCode: res.statusCode,
+    statusMessage: res.statusMessage,
+  }
+}
+var log = bunyan.createLogger({
+    name: 'copycat',
+    streams: [
+      {
+        level: 'trace',
+        stream: process.stdout  // log TRACE and above to stdout
+      },
+      {
+        level: 'debug',
+        path: './log/copycat-debug.log' // log DEBUG and above to a file
+      },
+      {
+        level: 'info',
+        path: './log/copycat-info.log' // log INFO and above to a file
+      },
+      {
+        level: 'error',
+        path: './log/copycat-error.log' // log ERROR and above to a file
+      },
+    ],
+    serializers: {
+      err: bunyan.stdSerializers.err,
+      req: reqSerializer,
+      res: resSerializer,
+      photo: photoSerializer,
+      album: albumSerializer,
+      editor: editorSerializer,
+      user: userSerializer,
+    },
+});
+module.exports.log = log;
+
+module.exports.logReqIdMiddleware = function logReqIdMiddleware(req, res, next) {
+  req.log = log.child({reqId: uuid.v1()});
+  next();
+};
+module.exports.logReq = function logReq(log, req) {
+  log.info({req:req}, "New request");
+}
+module.exports.logRes = function logReq(log, res) {
+  log.info({res:res}, "New response");
+}
+
+/**
+ * PromiseReject, used in Promise to as reject method
+ */
+function PromiseReject(message) {
+    this.name = 'MyError';
+    this.message = message;
+    this.stack = (new Error()).stack;
+}
+PromiseReject.prototype = new Error; 
+module.exports.PromiseReject = PromiseReject;
+
+
+
+
+
+
+
--- api/v0/photos.js
+++ api/v0/photos.js
@@ -4,119 +4,152 @@ var express = require("express");
 var router = express.Router();
 var models = require("../../database/v0/models.js");
 var bodyParser = require('body-parser');
+
+/**
+ * AWS service
+ */
 var AWS = require('aws-sdk');
 AWS.config.loadFromPath('./crenditial.json');
 var s3 = new AWS.S3();
 
-var base64 = require('base64-stream');
-var zlib = require('zlib');
-
+/**
+ * helper functions and objects
+ */
 var helper = require("./helper.js");
 var assertHeader = helper.assertHeader;
 var errHandle = helper.errHandle;
+var PromiseReject = helper.PromiseReject;
 
+/**
+ * log objects and functions
+ */
+var log = helper.log;
+var logReq = helper.logReq;
+var logRes = helper.logRes;
+var logReqIdMiddleware = helper.logReqIdMiddleware;
+
+/**
+ * compress image
+ */
 var fs = require('fs')
   , gm = require('gm').subClass({imageMagick: true});
 
+/**
+ * Add reqId to each request
+ */
+router.use(logReqIdMiddleware);
+
+/**
+ * Get a photo speficied by a photoId
+ */
 router.route('/:id')
 .get(function(req, res) {
+  logReq(req.log, req);
+
   var id = req.params.id;
-  models.Photo.findById(id, function(err, photo) {
-    if(err) return errHandle.unknown(res, err);
-    if(!photo) return errHandle.notFound(res, err);
-    res.send(photo);
+
+  //find photo by id
+  models.Photo.findById(id)
+  //assure photo exists
+  .then(function(photo) {
+    if(!photo) {
+      var msg = "Photo not found when get photo by id";
+      req.log.warn(msg);
+      errHandle.notFound(res, msg);
+      throw new PromiseReject();
+    }
+
+    req.log.info({photo:photo}, "Photo found");
+    return photo;
   })
+  //respond
+  .then((photo) => {
+      res.send(photo);
+      logRes(req.log, res);
+  })
+  //error handling
+  .catch(function(err) {
+    if(!(err instanceof PromiseReject)) {
+      req.log.error({err:err}, "Unknown error");
+      errHandle.unknown(res, err);
+    }
+  });
 });
 
-router.route('/:id/stream')
-.put(function(req, res, next) {
-  var id = req.params.id;
-  var fakeUserId;
-  var getFakeUserId = function() {
-    models.User.findOne(function(err, user) {
-      if(err) return errHandle.unknown(res, err);
-      fakeUserId = user._id;
-      uploadImage(id, updatePhoto);
-    })
-  }
-
-  var uploadImage = function(id, callback) {
-    var params = {
-      Bucket: 'copycatimage',
-      Key: id,
-      ACL: 'public-read',
-      Body: req.pipe(base64.decode()).pipe(zlib.createGzip()),
-      ContentEncoding: 'gzip',
-      ContentType: 'image/jpeg'
-    };
-    s3.upload(params)
-    .send(function(err, data) {
-      if (err) return errHandle.unknown(res, err);
-      callback(id, data.Location);
-    });
-  }
-
-  var updatePhoto = function(id, url) {
-    models.Photo.findByIdAndUpdate(
-      id, 
-      { $set: { imageUrl : url}},
-      {new : true},//set true to return modified data
-      complete
-    );
-  }
-
-  var complete = function(err, photo) {
-    if(err) return errHandle.unknown(res, err);
-    res.statusCode = 201;
-    res.send(photo);
-  }
-
-  getFakeUserId();
-});
-
-router.use(bodyParser.json({limit: '5mb'}));
+/**
+ * Post a photo given base64 image data
+ */
+router.use(bodyParser.json({limit: '5mb'}));//photo should no more than 5mb
 router.route('/')
 .post(function(req, res, next) {
-  assertHeader(req, res, 'content-type', 'application/json');
+  logReq(req.log, req);
 
+  assertHeader(req, res, req.log, 'content-type', 'application/json');
+
+  //get data part, data is base64 encoded
   var data = req.body.data;
-  if(!data) return errHandle.badRequest(res, 'missing data part in request');
+  if(!data) {
+    req.log.info("Missing data part in request.");
+    return errHandle.badRequest(res, 'Missing data part in request');
+  }
 
+  //get user id of the new photo
   var fakeUserId;
   var getFakeUserId = function() {
     models.User.findOne(function(err, user) {
-      if(err) return errHandle.unknown(res, err);
+      if(err) {
+        req.log.error({err:err}, "Error when get user id.");
+        return errHandle.unknown(res, err);
+      }
+      req.log.info({user:user}, "Found photo user.")
+
       fakeUserId = user._id;
-      postPhoto();
+      createPhoto();
     })
   }
 
+  //create a new empty photo(i.e. without imageUrl) in database,
+  //to get photoId
   var key;
-  var postPhoto = function() {
-    var photo = req.body;
+  var createPhoto = function() {
+    //create new photo
+    var photo = {}
+    photo.referenceId = req.body.referenceId;
+    //photo.ownerId = req.body.ownerId;
+    photo.tagList = req.body.tagList;
     photo.ownerId = fakeUserId;
-    delete photo.data
 
     models.Photo.create(photo, function(err, photo) {
-      if(err) return errHandle.unknown(res, err);
+      if(err) {
+        req.log.error({err:err}, "Error when create new photo.");
+        return errHandle.unknown(res, err);
+      }
+      req.log.info({photo:photo}, "Created new empty photo.");
+
       key = '' + photo._id;
       compress();
     });
   }
 
+  //compress photo data
   var compress = function() {
-    //get base64 data
     data = new Buffer(data, 'base64');
 
     gm(data, 'img')
     .resize(800, 600)
     .quality(80)
     .toBuffer('JPG', function(err, buffer) {
-      if(err) return errHandle.unknown(res, err);
+      if(err) {
+        req.log.error({err:err}, "Error when compress photo.");
+        return errHandle.unknown(res, err);
+      }
+      req.log.info("Compressed new photo.");
+
       uploadImage(buffer);
     })
   }
 
+  //upload new photo to AWS S3
   var uploadImage = function(buffer) {
     var params = {
       Bucket: 'copycatimage',
@@ -128,15 +161,26 @@ router.route('/')
     };
     s3.upload(params)
     .send(function(err, data) {
-      if (err) return errHandle.unknown(res, err);
+      if (err) {
+        req.log.error({err:err}, "Error when upload photo to S3.");
+        return errHandle.unknown(res, err);
+      }
+      req.log.info("Uploaded new photo to S3");
+
       updatePhoto(data.Location);
     });
   }
 
+  //get photo size info
   var updatePhoto = function(url) {
     gm(data, 'img')
     .size(function(err, size) {
-      if(err) return errHandle.unknown(res, err);
+      if(err) {
+        req.log.error({err:err}, "Error when get size of photo.");
+        return errHandle.unknown(res, err);
+      }
+      req.log.info("Got size of new photo.");
+
       models.Photo.findByIdAndUpdate(
         key,
         { $set: { imageUrl : url, width: size.width, height: size.height}},
@@ -146,13 +190,21 @@ router.route('/')
     })
   }
 
+  //update photo imageUrl, size info to database
   var complete = function(err, photo) {
-    if(err) return errHandle.unknown(res, err);
+    if(err) {
+      req.log.error({err:err}, "Error when update new photo.");
+      return errHandle.unknown(res, err);
+    }
+    req.log.info({photo:photo}, "Updated new photo.");
+
     res.statusCode = 201;
     res.send(photo);
+    logRes(req.log, res);
   }
 
   getFakeUserId();
 });
+
 module.exports = router;
 
