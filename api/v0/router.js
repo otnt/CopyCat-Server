593c9c1f9b2e86011b6718ac1a099058a5b10717
Ubuntu <ubuntu@ip-172-31-51-86.ec2.internal>
2016-04-02 17:56:28 +0000

    heartbeat for load balancer

--- api/v0/router.js
+++ api/v0/router.js
@@ -2,11 +2,13 @@
 
 var express = require("express");
 var router = express.Router();
+
 var promos = require("./promos.js");
 var albums = require("./albums.js");
 var photos = require("./photos.js");
 var labels = require("./labels.js");
 var timeline = require("./timeline.js");
+var heartbeat = require("./heartbeat.js");
 
 router.use(function(req, res, next) {
   console.log(Date.now() + " " + req.originalUrl);
@@ -18,5 +20,6 @@ router.use('/albums', albums);
 router.use('/photos', photos);
 router.use('/labels', labels);
 router.use('/timeline', timeline);
+router.use('/heartbeat', heartbeat);
 
 module.exports = router;
