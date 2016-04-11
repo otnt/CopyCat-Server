3304fd56ed990eee980b29a1d5a13e73e242f5c9
Pufan Jiang <jiangpufan@gmail.com>
2016-04-11 03:21:58 +0000

    modify feedback test

--- test/test_feedback.py
+++ test/test_feedback.py
@@ -8,6 +8,7 @@ data=json.dumps({\
         'text': 'I have a feedback!' \
         })
 
-r = requests.post('http://52.90.52.232/api/v0/feedback/', data=data,
+#r = requests.post('http://52.90.52.232/api/v0/feedback/', data=data,
+r = requests.post('http://127.0.0.1/api/v0/feedback/', data=data,
         headers = {'content-type': 'application/json', 'content-length' : len(data)})
 print r.text
