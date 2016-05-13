f918b3e244e2c122bddc3b3120c084faaa27daef
pufan jiang <jiangpufan@gmail.com>
2016-05-13 09:16:07 +0000

    test image

--- test/test_upload_photo.py
+++ test/test_upload_photo.py
@@ -3,9 +3,10 @@ import json
 import sys
 
 buf = ''
-for line in sys.stdin.readline():
+for line in sys.stdin:
     buf += line.strip()
 
-    r = requests.post('http://CopyCatLoadBalancer-426137485.us-east-1.elb.amazonaws.com/api/v0/photos/', data=json.dumps({'data':buf}), \
-            headers = {'content-type': 'application/json', 'content-length' : len(buf)})
-    print r.text
+#r = requests.post('http://CopyCatLoadBalancer-426137485.us-east-1.elb.amazonaws.com/api/v0/photos/', data=json.dumps({'data':buf}), \
+print len(buf)
+r = requests.post('http://ec2-54-82-236-201.compute-1.amazonaws.com/api/v0/photos/', data=json.dumps({'data':buf, 'ownerId':'56d1fa5043667c107ded881f'}), headers = {'content-type': 'application/json', 'content-length' : len(buf)})
+print r.text
