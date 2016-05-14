a0f0b6a12f70b3371691ccf1ed0fc2efb6e9f708
pufan jiang <jiangpufan@gmail.com>
2016-05-14 04:49:29 +0000

    modify upload test

--- test/test_upload_photo.py
+++ test/test_upload_photo.py
@@ -6,7 +6,7 @@ buf = ''
 for line in sys.stdin:
     buf += line.strip()
 
-#r = requests.post('http://CopyCatLoadBalancer-426137485.us-east-1.elb.amazonaws.com/api/v0/photos/', data=json.dumps({'data':buf}), \
 print len(buf)
-r = requests.post('http://ec2-54-82-236-201.compute-1.amazonaws.com/api/v0/photos/', data=json.dumps({'data':buf, 'ownerId':'56d1fa5043667c107ded881f'}), headers = {'content-type': 'application/json', 'content-length' : len(buf)})
+r = requests.post('http://CopyCatLoadBalancer-426137485.us-east-1.elb.amazonaws.com/api/v0/photos/', data=json.dumps({'data':buf, 'ownerId':'571e8f58fd825f2e17cfcccf'}), headers = {'content-type': 'application/json', 'content-length' : len(buf)})
+#r = requests.post('http://ec2-54-82-236-201.compute-1.amazonaws.com/api/v0/photos/', data=json.dumps({'data':buf, 'ownerId':'571e8f58fd825f2e17cfcccf'}), headers = {'content-type': 'application/json', 'content-length' : len(buf)})
 print r.text
