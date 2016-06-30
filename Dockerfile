f5b00e1722be28dd7be973b71f71db145a931fda
pufan jiang <jiangpufan@gmail.com>
2016-06-30 11:40:43 -0700

    refine login

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,5 @@
 FROM node:latest
 
-#COPY . /workspace
-#WORKDIR /workspace
-
-## Provides cached layer for node_modules
-#ADD package.json /tmp/package.json
-#RUN cd /tmp && npm install
-#RUN mkdir -p /src && cp -a /tmp/node_modules /src/
-#
-## Define working directory
-#WORKDIR /src
-#ADD . /src
-
 WORKDIR /workspace
 ADD package.json /workspace
 RUN npm install
@@ -21,6 +9,4 @@ ADD . /workspace
 EXPOSE 3001
 
 # Run app using nodemon
-#CMD ["npm", "start"]
-
 CMD ["npm", "start"]
---
+++ utils/login.js
@@ -0,0 +1,51 @@
+const models = require('../../../database/v0/models.js');
+const util = require('util');
+
+/**
+ * Generic login mechanism.
+ *
+ * userInfo: Used to search a user in our database.
+ * defaultUserInfo: Used to create a user in our database if
+ *                  previous search failed.
+ * callback: User registered function to give controll back to
+ *           caller after search user.
+ */
+const login = function login(userInfo, defaultUserInfo, callback) {
+  // Find user if it exists
+  models.User.find(userInfo)
+  .then((user) => {
+    if (!user || user.length === 0) {
+      return models.User.create(defaultUserInfo);
+    }
+    return user;
+  })
+  // Assert user create succeed and return
+  .then((user) => {
+    if (!user || user.length === 0) {
+      callback(new Error(util.format('Create copycat user %j failed', defaultUserInfo)), null);
+    } else {
+      callback(null, user[0]);
+    }
+  })
+  // error handling
+  .catch((err) => { callback(err); });
+};
+
+/**
+ * Let instagram user login. We save a corresponding copycat user in our
+ * database using instagram user id as key. If no such user exist, we create
+ * a user for him/her.
+ */
+const instagram = function instagram(instagramInfo, callback) {
+  const instagramUser = instagramInfo.user;
+  const defaultUserInfo = {
+    name: instagramUser.username,
+    profilePictureUrl: instagramUser.profile_picture,
+    instagramId: instagramUser.id,
+  };
+
+  login(instagramInfo, defaultUserInfo, callback);
+};
+
+
+module.exports.instagram = instagram;
